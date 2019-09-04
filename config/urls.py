ddcd2681ffe2348936baa001e72b5027fb68133b
Jongwoo-Han <han980817@gmail.com>
2019-09-04 11:27:29 +0900

    Added Template: Home

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import crawler.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', crawler.views.home, name="home"),
 ]
